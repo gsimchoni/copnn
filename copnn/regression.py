fe000476415b9eecfffa625a69e143cb40596c36
gsimchoni <gsimchoni@gmail.com>
2024-09-28 10:54:05 +0300

    Avoid pandas warning when concatenating empty df

--- copnn/regression.py
+++ copnn/regression.py
@@ -61,7 +61,10 @@ def process_one_hot_encoding(X_train, X_test, x_cols):
         X_train_cols_not_in_test = set(X_train_ohe.columns).difference(X_test_ohe.columns)
         X_test_comp = pd.DataFrame(np.zeros((X_test.shape[0], len(X_train_cols_not_in_test))),
             columns=list(X_train_cols_not_in_test), dtype=np.uint8, index=X_test.index)
-        X_test_ohe_comp = pd.concat([X_test_ohe[list(X_test_cols_in_train)], X_test_comp], axis=1)
+        if not X_test_comp.empty:
+            X_test_ohe_comp = pd.concat([X_test_ohe[list(X_test_cols_in_train)], X_test_comp], axis=1)
+        else:
+            X_test_ohe_comp = X_test_ohe[list(X_test_cols_in_train)]
         X_test_ohe_comp = X_test_ohe_comp[X_train_ohe.columns]
         X_train_ohe.columns = list(map(lambda c: z_col + '_' + str(c), X_train_ohe.columns))
         X_test_ohe_comp.columns = list(map(lambda c: z_col + '_' + str(c), X_test_ohe_comp.columns))
